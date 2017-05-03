61bb1b7dc1578a6d74f3ee2eda4c4c17c9439be2
jonatahessa <jonata.hessa@gmail.com>
2017-05-03 13:16:24 -0300

    form pessoa fisica funcional

--- domPessoaFisica.js
+++ domPessoaFisica.js
@@ -1,3 +1,23 @@
+function main() {
+  var btn = document.querySelector("#imprimir");
+  var btn2 = document.querySelector("#confirma");
+  btn.addEventListener("click", changeCSS);
+  btn2.addEventListener("click", confirma);
+}
+
+function changeCSS() {
+  var lnk = document.querySelector("link");
+  lnk.setAttribute('rel', "stylesheet" );
+  lnk.setAttribute('href', "estiloPessoaFisicaPrint.css" );
+  var head = document.querySelector("head");
+  head.appendChild(lnk);
+}
+
+function confirma() {
+  document.querySelector("#botao").style.display = 'none';
+  window.print();
+}
+
 function mascara(t, mask) {
   var i = t.value.length;
   var saida = mask.substring(1,0);
